6904f079f6001da5fa71df6798553c49c85a1825
itw_jiayc01 <yuanchao__2106@163.com>
2019-11-05 09:48:08 +0800

    Initial Commit

---
+++ JYCClassicAlgorithm/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JYCClassicAlgorithm
+//
+//  Created by 贾远潮 on 2019/11/5.
+//  Copyright © 2019 贾远潮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ JYCClassicAlgorithm/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  JYCClassicAlgorithm
+//
+//  Created by 贾远潮 on 2019/11/5.
+//  Copyright © 2019 贾远潮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
